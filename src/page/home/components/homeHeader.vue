f45d3b6907f53880709201672ef2ecbb83d562b4
leolonger <1753081223@qq.com>
2019-04-03 18:01:56 +0800

    home-header

--- src/page/home/components/homeHeader.vue
+++ src/page/home/components/homeHeader.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="test">homeHeader</div>
+  <div class="header">
+  	<div class="header-back">
+  		<span class="iconfont">&#xe641;</span>
+  	</div>
+  	<div class="header-search">
+  	  <span class="iconfont">&#xe7b7;</span>上海海昌海洋公园
+  	</div>
+  	<div class="header-city">
+  	北京
+  	<span class="iconfont">&#xe651;</span></div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +19,23 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.test
-    color:red
+.header
+  line-height:.88rem
+  text-align:center
+  display:flex
+  .header-back
+    float:left
+    width:.9rem
+  .header-search
+    flex:1
+    height:.6rem
+    line-height:.6rem
+    margin-top:.2rem
+    margin-left:.2rem
+    margin-right:.2rem
+    border-radius:.5rem
+    box-shadow:0 0 .01rem .02rem #005
+  .header-city
+    float:right
+    width:1.4rem
 </style>
